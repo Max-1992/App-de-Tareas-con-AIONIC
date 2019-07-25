0d45fb5063a51e758bba8dc4835255d2615cac51
Maximiliano Echevarria <maximiliano.echevarria@hotmail.com>
2019-07-25 11:00:39 -0300

    Se mejoraron detalles esteticos con animaciones y se agrego la funcionalidad del checkbox para determinar cuando una tarea eta finalizada y cuando una lista de tareas fue completada

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -39,7 +39,22 @@ export class AgregarPage {
           this.tareasService.guardarStorage();
 
       }
+      
+    public cambioCheck( item:ListaItem ){
+        console.log(item);
 
+        const pendientes = this.lista.items.filter( itemData =>  itemData.completado === false ).length;
+
+        if (  pendientes === 0 ){
+                this.lista.terminadaEn = new Date();
+                this.lista.terminada = true;
+        } else {
+                this.lista.terminadaEn = null;
+                this.lista.terminada = false;
+        }
+      
+        this.tareasService.guardarStorage();
+    }
       
    }
 
